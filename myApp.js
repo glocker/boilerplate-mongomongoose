c1bcbf2bc905089bee36a75e0944d176d3e736dc
Egor Rakitin <georgeglocker@gmail.com>
2024-04-22 20:00:00 +0000

    Connected db, created model

--- myApp.js
+++ myApp.js
@@ -1,10 +1,31 @@
-require('dotenv').config();
+// Install and set up mongoose 
 
+const mongoose = require('mongoose');
+require('dotenv').config()
 
-let Person;
+// Connect to mongo db
+mongoose.connect(process.env.MONGO_URI, 
+  { useNewUrlParser: true, useUnifiedTopology: true });
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+// Create a Person Model
+var personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favoriteFoods: [String]
+});
+
+// Create and Save a Person
+var Person = mongoose.model('Person', personSchema);
+
+var createAndSavePerson = function(done) {
+  var janeFonda = new Person({name: "Jane Fonda",
+   age: 84,
+   favoriteFoods: ["eggs", "fish", "fresh fruit"]});
+
+  janeFonda.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+  });
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
